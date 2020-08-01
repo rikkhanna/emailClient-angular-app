4a6e003a2379667b3165384d7f7001f23eabbcf7
Rishabh Khanna <rishabhkhanna726@gmail.com>
2020-08-02 00:22:41 +0530

    checking auth status using withCredentials: true

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -33,12 +33,24 @@ export class AuthService {
 
   signup(credentials: SignupCredentials){
     return this.http.post<SignupResponse>(
-      this.rootUrl+'/auth/signup', credentials
+      this.rootUrl+'/auth/signup', credentials, {
+        withCredentials: true
+      }
       ).pipe(
         tap(() => {
           this.signedIn$.next(true);
         })
       )
+  }
+
+  checkAuth(){
+    return this.http.get(this.rootUrl+'/auth/signedin',{
+      withCredentials: true
+    }).pipe(
+      tap(response => {
+        console.log(response);
+      })
+    )
   }
 
 }
