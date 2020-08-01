2df45e0e631079b2b3211c2aa32c36ad3337a567
Rishabh Khanna <rishabhkhanna726@gmail.com>
2020-08-02 00:34:31 +0530

    http Interceptor

--- src/app/auth/auth-http-interceptor.ts
+++ src/app/auth/auth-http-interceptor.ts
@@ -1,2 +1,17 @@
-export class AuthHttpInterceptor {
+import {Injectable} from '@angular/core';
+import {
+    HttpEvent,
+    HttpInterceptor,
+    HttpHandler,
+    HttpRequest
+} from '@angular/common/http';
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthHttpInterceptor implements HttpInterceptor{
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        throw new Error("Method not implemented.");
+    }
 }
