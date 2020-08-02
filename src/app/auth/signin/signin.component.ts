e98743047c9a5ec9ff924fb43c7436fb6c41d5a8
Rishabh Khanna <rishabhkhanna726@gmail.com>
2020-08-02 17:34:06 +0530

    building sign in flow

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
+  /** create a new auth form  */
+  authForm = new FormGroup({
+
+    /** list out the diff. fields for signin form */
+    username: new FormControl('',[
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.pattern(/^[a-z0-9]+$/)
+    ]),
+    password: new FormControl('',[
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20)
+    ])
+  });
   constructor() { }
 
   ngOnInit(): void {
