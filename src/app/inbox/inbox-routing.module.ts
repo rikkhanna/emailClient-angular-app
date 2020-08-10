91817eefcde933114275862fed3c7814a11ed390
Rishabh Khanna <rishabhkhanna726@gmail.com>
2020-08-10 20:29:05 +0530

    child routes

--- src/app/inbox/inbox-routing.module.ts
+++ src/app/inbox/inbox-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { EmailShowComponent } from './email-show/email-show.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent}
+  { 
+    path: '', 
+    component: HomeComponent,
+    children:[
+      {
+        path:'',
+        component: PlaceholderComponent
+      },
+      {
+        path:':id', //capture any route that looks like /inbox/id 
+        // colon is sort of a wildcard and  specify that we want to capture any variable or any string whatsoever and it as variable id
+        component: EmailShowComponent
+      },
+    ] 
+  }
 ];
 
 @NgModule({
